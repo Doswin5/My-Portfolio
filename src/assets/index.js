ebbd32035f4e9d3b26f2a4df3b18dcd60d4110d5
DOSWIN <ayomidedosunmuoreoluwa@gmail.com>
2022-12-16 03:58:21 +0100

    Resume PDF Download

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,7 @@ import sun from './sun.png'
 import moon from './moon.png'
 import menu from './menu.svg'
 import close from './close.svg'
+import download from './download.png'
+import resume from './resume.pdf'
 
-export { sun, moon, menu, close };
+export { sun, moon, menu, close, download, resume };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { resume, download } from '../assets'
 
 const Hero = () => {
   return (
     <div className="flex flex-col h-[70.5vh] justify-center">
       <h3>Hi, i am</h3>
-      <h2 className='text-primary'>DOSUNMU AYOMIDE.</h2>
+      <h2 className='text-primary'>
+        DOSUNMU AYOMIDE.
+      </h2>
       <h3>A Frontend Web Developer.</h3>
+      <div className='mt-8'>
+        <a 
+          href={resume} 
+          download='Ayomide reume.pdf'
+          className='btn flex flex-row items-center'
+        >
+          Download CV
+          <img 
+            src={download} 
+            alt="download" 
+            className='w-[24px] h-[24px] inline ml-2'
+          />
+        </a>
+
+      </div>
+
     </div>
   )
 }
