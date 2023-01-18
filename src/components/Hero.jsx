99d49b2f917396c07889174cca66e8dd33dac3bf
DOSWIN <ayomidedosunmuoreoluwa@gmail.com>
2023-01-18 16:37:30 +0100

    Revamp on the Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <div className="flex flex-col h-[70.5vh] justify-center pb-8 sm:pt-[150px] pt-[120px]">
       <h3>Hi, i am</h3>
-      <h2 className='text-primary'>
+      <h2 className='text-[18px] sm:text-[24px] '>
         DOSUNMU AYOMIDE.
       </h2>
-      <h3>A Frontend Web Developer.</h3>
+      <h3 className='text-primary text-[28px] sm:text-[40px]'>A Frontend Web Developer.</h3>
       <div className='mt-4 flex items-center gap-4'>
         <Link to='/contact'>
           <button className='hover:bg-primaryHover'>Hire Me</button>
